909b725654823529799b6f66d1f63fa531d62ebb
fdesi <francesco.desimone@canonical.com>
2025-02-04 19:47:42 +0100

    update cloud-init

--- vms.tf
+++ vms.tf
@@ -17,8 +17,11 @@ users:
     - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCKZc6GLae5vmRYjb78j1jJaHyCqMcQcTiEBJpVSIyHsjKS9UoGCDe3I8MXrkB+cdTCGexG+xifKi/ZoxgcJyIRx3dZgdTNiDwmgMsSqpxBuyhBWFMLM0GWWXcK48VCBxOADNgGepTVv28tzaCXYFNJfFpeq+pmveU84m3258JD60fAMyn4pxyokMoWanZ0KRHE+B/TIfRW4X8M+GXr//aAgAR5YVCM/zNhEfojrGMV5eGMzgPPaZLA719Z2sNqvZ/pSGUM+x8jLv4oTrUvmStNhtpicoIMDOwlHalHtxpJ4Cda6U5cggpfQiUppyhSBbhVBvyFLLH7zmUuRckmQdew47W9o2EAIHJ6KNEQ1UcbhkCrpxXkHwYE2SNnXE+We84uLR7D+HJzcIq2QD5akHSvaacOyH5GCEziRvMNNNwtHCfh7m8bUaZmSCCbvfkgpCNM6z5XOP1Zc79tLcMbXIusHTZ+wwMuugn6IW6HVZdED5upbCZTQlt6um9m+u4+zI0= fdesi@fdesi-ws
     sudo: ALL=(ALL) NOPASSWD:ALL
 runcmd:
-- sudo snap install openstack --channel 2024.1/beta
-- sunbeam prepare-node-script --bootstrap
+- su ubuntu -c "sudo snap install openstack --channel 2024.1/beta"
+- su ubuntu -c "sudo systemctl start user@1000"
+- su ubuntu -c "sudo -i -u ubuntu sunbeam prepare-node-script --bootstrap  bash -x"
+- su ubuntu -c "sudo -i -u ubuntu sunbeam cluster bootstrap --accept-defaults"
+
 EOT
 }
 
